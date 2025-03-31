ae30625c7322d894a2657cc546d0f95d1324c2e0
Deepak Nagaraj <deepaknagaraj1997@gmail.com>
2025-03-31 04:58:39 +0000

    Added

--- lib/openproject/sample_plugin/engine.rb
+++ lib/openproject/sample_plugin/engine.rb
@@ -1,6 +1,15 @@
-module Openproject
+module OpenProject
   module SamplePlugin
     class Engine < ::Rails::Engine
+      engine_name :sample_plugin
+
+      initializer 'sample_plugin.register_hooks' do
+        require_relative 'hooks'
+      end
+
+      config.to_prepare do
+        # Add any necessary preparation code here
+      end
     end
   end
 end
